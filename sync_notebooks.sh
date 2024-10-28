3b27a5923ee4a37bd1035a6293a52511194f9624
Praveen Roy <praveen.roy1991@gmail.com>
2024-10-28 19:00:18 +0530

    Auto-synced updates from Jupyter Notebook

--- sync_notebooks.sh
+++ sync_notebooks.sh
@@ -1,4 +1,3 @@
-cd /Users/praveenroy/PycharmProjects/Jupyter-Notebook # Replace with the actual path to your Jupyter notebook folder
 git add .
 git commit -m "Auto-synced updates from Jupyter Notebook"
 git push origin main  # Replace 'main' with 'master' if your branch is named 'master'
